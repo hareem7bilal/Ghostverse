a01e9b2550cf3413d7cd8fc2f2ca30692d539a13
hareem7bilal <hareem7bilal@gmail.com>
2023-07-15 06:42:14 -0700

    auth, nav

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { User } from 'next-auth'
+import Link from 'next/link'
+import { FC } from 'react'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuItem} from './ui/DropdownMenu'
+import UserAvatar from './UserAvatar'
+import { signOut } from "next-auth/react"
+
+interface UserAccountNavProps {
+    user: Pick<User, 'name' | 'image' | 'email'>
+}
+
+const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
+    return <DropdownMenu>
+        <DropdownMenuTrigger><UserAvatar className='h-10 w-10' user={{
+            name: user.name || null,
+            image: user.image || null
+        }} /></DropdownMenuTrigger>
+
+        <DropdownMenuContent className='bg-white' align='end'>
+            <div className='flex items-center justify-start gap-2 p-2'>
+                <div className='flex flex-col space-y-1 leading-none'>
+                    {user.name && <p className='font-medium'>{user.name}</p>}
+                    {user.email && <p className='text-sm w-[200px] truncate text-zinc-700'>{user.email}</p>}
+                </div>
+            </div>
+
+            <DropdownMenuSeparator/>
+            
+            <DropdownMenuItem asChild>
+                <Link href='/'>Feed</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem asChild>
+                <Link href='/r/create'>Create Community</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem asChild>
+                <Link href='/settings'>Settings</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator/>
+
+            <DropdownMenuItem onSelect={(event)=>{
+                event.preventDefault()
+                signOut({
+                    callbackUrl: `${window.location.origin}/sign-in`
+                })
+
+            }} className='cursor-pointer'>
+               Sign out
+            </DropdownMenuItem>
+
+        </DropdownMenuContent>
+    </DropdownMenu>
+}
+
+export default UserAccountNav
